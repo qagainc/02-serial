22f0bac59770c97fc5ed1c6650b5ef0c2530bd53
qagainq <751910630@qq.com>
2018-06-08 13:55:15 +0800

    a3

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ elif platform.system() == "Linux":
 import tkinter as tk
 import tkinter.ttk
 
-a1=1;
-a2=2;
+a1 =1;
+a2 =2;
+a3 =3;
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S')
